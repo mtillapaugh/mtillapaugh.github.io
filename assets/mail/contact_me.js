a2333a3312d70c6ef1235c91b6244b760a8f6798
mtillapaugh <mtillapaugh@gmail.com>
2020-08-17 15:44:26 -0700

    Contact form fix v2

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -21,7 +21,7 @@ $(function () {
             $this = $("#sendMessageButton");
             $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
             $.ajax({
-                url: "//formspree.io/"mtillapaugh@gmail.com",
+                url: "//formspree.io/mtillapaugh@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
